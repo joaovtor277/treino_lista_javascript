f7722707d66e6a35d3919c3a884778063f912d66
joaovtor277 <freitaslima155@gmail.com>
2025-08-26 00:05:03 -0300

    to do list finalizada

--- main.js
+++ main.js
@@ -7,6 +7,22 @@ function adicionarTarefa() {
     if (tarefa !== ""){
         const li = document.createElement('li');
         li.textContent = tarefa;
+
+        li.addEventListener('click', function(){
+            li.classList.toggle("concluida")
+        });
+
+        const btnRemover = document.createElement('button');
+        btnRemover.textContent = "Remover";
+        btnRemover.style.marginLeft = "100px"
+
+        btnRemover.addEventListener('click', function(event){
+            event.stopPropagation();
+            li.remove();
+        });
+
+        li.appendChild(btnRemover);
+
         lista.appendChild(li);
         input.value = "";
     } else {
